51a4a3b9da92e48aa87297c1910fa91f37320d10
yusluv1234 <yusluv124@gmail.com>
2022-11-09 15:03:59 +0100

    Strating Stripe  payment with node

--- stripepaymentwithnode/app.js
+++ stripepaymentwithnode/app.js
@@ -1,14 +1,14 @@
-// const http = require('http')
-// const port = 5000
+const http = require('http')
+const port = 5000
 
-// const server = http.createServer(function(req, res) {
-//   Response.write('Hello Node')
-// })
+const server = http.createServer(function(req, res) {
+  Response.write('Hello Node')
+})
 
-// server.listen(port, function(error) {
-//   if (error) {
-//     console.log('somethingwent wrong', error)
-//   } else {
-//     console.log('Server is listening on port ' + port)
-//   }
-// })
+server.listen(port, function(error) {
+  if (error) {
+    console.log('somethingwent wrong', error)
+  } else {
+    console.log('Server is listening on port ' + port)
+  }
+})
